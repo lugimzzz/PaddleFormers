e2086671c850f75dcd172e7e74e77503810cc8a6
Liujie0926 <44688141+Liujie0926@users.noreply.github.com>
2025-06-26 14:17:27 +0800

    [CI] Update branch and repo_name for ci (#2373)

--- scripts/unit_test/ci_unit.sh
+++ scripts/unit_test/ci_unit.sh
@@ -17,8 +17,8 @@
 set -e
 export paddle=$1
 export FLAGS_enable_CE=${2-false}
-export nlp_dir=/workspace/PaddleHub
-export log_path=/workspace/PaddleHub/unittest_logs
+export nlp_dir=/workspace/PaddleFormers
+export log_path=/workspace/PaddleFormers/unittest_logs
 cd $nlp_dir
 
 if [ ! -d "unittest_logs" ];then
