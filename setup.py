d7a554a5e14b6ef93017b68e4a2843fe512346c5
Lugim <63761690+lugimzzz@users.noreply.github.com>
2025-09-11 12:09:00 +0800

    update v0.2.4 (#2591)
    Co-authored-by: Ace-To-HYB <huangyb83@foxmail.com>

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ def show():
 
 
 # only use this file to contral the version
-__version__ = "0.2.3.post"
+__version__ = "0.2.4.post"
 if os.getenv(PADDLEFORMERS_STABLE_VERSION):
     __version__ = __version__.replace(".post", "")
 else:
